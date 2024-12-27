937e0a6623362359e3fb75995f8e01e9966956ef
AhmedAli0123 <ahmedsiridab1@gmail.com>
2024-12-27 12:58:40 +0500

    Create A Blogging Website

---
+++ src/type/BlogDetail.ts
@@ -0,0 +1,14 @@
+export interface BlogDetailType {
+    id?: number;
+    title?: string;
+    subtitle?: string;
+    about?: string;
+    description?: string;
+    conclusion?: string;
+    image?: string;
+    date?: string;
+  author?: string;
+  introduction?: string;
+  content?: string;
+  tags?: string[];
+}
